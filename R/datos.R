54612c3a08643d9746ee7fc082126dc203ad7e00
Ezequiel Piseri <ezequielpiseri@gmail.com>
2025-10-24 10:20:56 -0300

    Documentacion de datos

---
+++ R/datos.R
@@ -0,0 +1,10 @@
+#'Datos aleatorios
+#'
+#'Set de datos de ejemplo con datos aleatorios
+#'
+#'@format vector de largo 1000
+#'
+#'\describe{
+#'  \item{datos}{numerico, de largo 1000.}
+#'}
+"datos"
